6e143fa7415b8ab71bb71a0e20121711f16ef5f1
Everton <everton8cosmo@gmail.com>
2018-05-18 01:38:36 -0300

    removida repetição de código

--- Programacao Orientada a Objetos/Fibonacci/Main.java
+++ Programacao Orientada a Objetos/Fibonacci/Main.java
@@ -14,17 +14,7 @@ class Main{
                                      
                }
 
-}                               int x =13;                               
-                    while(x != 1){
-                        if(x%2 == 0){
-                                x = x/2;
-                        }else{
-                                x = (3*x+1);
-                        }
-                        
-                    System.out.println(x);
-                    }
-                                     
-               }
+}                                
+               
 
-}
+
