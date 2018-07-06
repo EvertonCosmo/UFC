0241ad369a105c475978366ca0567077e6b3514f
Everton Cosmo <everton8cosmo@gmail.com>
2018-07-06 19:23:47 -0300

    added single bench

---
+++ Programacao Orientada a Objetos/Bank/src/com/cosmo/everton/entitys/Movement.java
@@ -0,0 +1,36 @@
+package com.cosmo.everton.entitys;
+
+
+
+public class Movement {
+
+	private String description;
+	private double values;
+	private String type; // if credit or debit 
+
+
+	public Movement(String description, double values, String type) {
+		this.description = description;
+		this.values = values;
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	public double getValues() {
+		return values;
+	}
+	public String getType() {
+		return type;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Movement [description=" + description + ", values=" + values + ", type=" + type + "]\n";
+	}
+
+
+
+}
